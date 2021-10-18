626b07347e8c9a3c896b74131ae0cce51e732920
2pichar <charles.ludden@gmail.com>
2021-10-18 16:09:32 +0000

    added user input methods

--- src/ts/user.ts
+++ src/ts/user.ts
@@ -1,1 +1,21 @@
-export default ''; // Starting export to mark as module
+import * as readline from 'readline';
+const rl: readline.Interface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+async function input(prompt: str): Promise<str> {
+  return new Promise((resolve, reject) => {
+    try{
+      rl.question(prompt, (answer)=>{
+        resolve(answer);
+      })
+    } catch(e){
+      reject(e);
+    }
+  });
+}
+function onInput(callback): void{
+  process.stdin.on('data', callback);
+}
+export default onInput;
+export {onInput, input};
