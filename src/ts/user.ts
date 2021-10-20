f3fa56515ba1fda9b546cd5d03790f17efe3d232
2pichar <charles.ludden@gmail.com>
2021-10-20 02:05:07 +0000

    Added User Interface class

--- src/ts/user.ts
+++ src/ts/user.ts
@@ -1,8 +1,12 @@
 import * as readline from 'readline';
+import * as events from 'events'; // Get events?
+import {Readable} from 'stream';
 const rl: readline.Interface = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
+  prompt: '>'
 });
+rl.prompt(true);
 async function input(prompt: str): Promise<str> {
   return new Promise((resolve, reject) => {
     try{
@@ -17,5 +21,21 @@ async function input(prompt: str): Promise<str> {
 function onInput(callback): void{
   process.stdin.on('data', callback);
 }
-export default onInput;
-export {onInput, input};
+
+class UI extends Readable {
+    private _src: any;
+    constructor(src) {
+        super({encoding: 'utf-8'});
+        this._src = src;
+        this._src.on('data', (data)=> {
+          this.push(data);
+          this.emit('input');
+        });
+    };
+    _read(size){
+        this._src.read(size);
+    }
+}
+
+export default UI;
+export {onInput, input, UI};
