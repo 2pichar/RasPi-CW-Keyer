822ba7c76d3f6e307a337a52e23cca96eca7a9c9
2pichar <charles.ludden@gmail.com>
2021-10-20 01:52:41 +0000

    Created beginning of radio interface

--- src/ts/radio.ts
+++ src/ts/radio.ts
@@ -1,1 +1,14 @@
-export default ''; // Starting export to mark as module
+import {Duplex} from 'stream';
+
+//Implement read, write, writev, destroy
+class radioInterface extends Duplex {
+    constructor(){
+        super({encoding: 'utf-8'});
+    }
+    _read(size: int){};
+    _write(chunk: str | Buffer, encoding: str, callback: Function){};
+    _writev(chunks: {chunk: str | Buffer, encoding: str}[], callback: Function){};
+    _destroy(err: Error, callback: Function){};
+};
+export default radioInterface;
+export {radioInterface};
