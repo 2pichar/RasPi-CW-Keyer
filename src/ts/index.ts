8d5466db361aee06e40b96bdc2b86551986d44f0
2pichar <charles.ludden@gmail.com>
2021-10-20 02:06:12 +0000

    Started main code

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,2 +1,13 @@
-import * as radio from './radio' // Import radio interface module
+import radioInterface from './radio' // Import radio interface module
 import * as user from './user' // Import UI module
+import * as morse from './morse' // Import morse code converter
+
+var radio = new radioInterface(); // Create radio interface
+radio.on('data', (data) => {
+    console.log(morse.from(data));
+});
+
+var ui = new user.UI(process.stdin); // Create UI
+ui.on('data', (data) => {
+   radio.write(morse.to(data)); 
+});
