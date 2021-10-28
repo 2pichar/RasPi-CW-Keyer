258c4388c1b2f0caed10e74745af3b1a4b658a27
2pichar <77556662+2pichar@users.noreply.github.com>
2021-10-28 13:00:12 -0700

    implemented _write function and created transmit and receive functions

--- src/ts/radio.ts
+++ src/ts/radio.ts
@@ -1,12 +1,23 @@
 import {Duplex} from 'stream';
 
+function transmitCW(): void{}
+function receiveCW(): str[]{}
+
 //Implement read, write, writev, destroy
 class radioInterface extends Duplex {
     constructor(){
         super({encoding: 'utf-8'});
     }
     _read(size: int){};
-    _write(chunk: str | Buffer, encoding: str, callback: Function){};
+    _write(chunk: str | Buffer, encoding: str, callback: Function){
+        let code = chunk.toString('utf-8');
+        try{
+            transmitCW([code]);
+            callback(null);
+        } catch(err){
+            callback(err);
+        }
+    };
     _writev(chunks: {chunk: str | Buffer, encoding: str}[], callback: Function){};
     _destroy(err: Error, callback: Function){};
 };
