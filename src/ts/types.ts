90a22b30da8ac8da71b668b0f871e50d96bfe907
2pichar <77556662+2pichar@users.noreply.github.com>
2021-10-29 11:23:03 -0700

    Added definitions for Object.prototype.keys and values

--- src/ts/types.ts
+++ src/ts/types.ts
@@ -2,3 +2,27 @@ type str = string
 type int = number
 type bool = boolean
 type date = Date
+
+Object.defineProperties(Object.prototype, {
+    keys: {
+        value(){
+            return Object.keys(this);
+        },
+        enumerable: false,
+        writable: false,
+        configurable: false
+    },
+    values: {
+        value(){
+            return Object.values(this);
+        },
+        enumerable: false,
+        writable: false,
+        configurable: false
+    }
+});
+
+interface Object<K, V> {
+    keys: () => K[];
+    values: () => V[];
+}
