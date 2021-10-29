7982b6b39f8feac989e75f2e5f3a2972f8e56e59
2pichar <77556662+2pichar@users.noreply.github.com>
2021-10-29 13:24:21 -0700

    Updated index to use stream pipelines for radio and user interfaces

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,13 +1,33 @@
 import radioInterface from './radio' // Import radio interface module
 import * as user from './user' // Import UI module
 import * as morse from './morse' // Import morse code converter
+import * as stream from 'stream'
 
 var radio = new radioInterface(); // Create radio interface
-radio.on('data', (data) => {
-    console.log(morse.from(data));
-});
+/*radio.on('data', (data) => {
+    ui.write(morse.from(data));
+});*/
 
-var ui = new user.UI(process.stdin); // Create UI
-ui.on('data', (data) => {
+var ui = new user.UI(process.stdin, process.stdout); // Create UI
+/*ui.on('data', (data) => {
    radio.write(morse.to(data)); 
-});
+});*/
+pipeline( // user input pipeline
+    ui,
+    async function*(data){
+        for await (let i of data) {
+            yield morse.to(i);
+    }},
+    radio,
+    (err => {(err)? console.error(err): console.log('complete');})
+);
+pipeline( // radio input pipeline
+    radio,
+    async function*(data){
+        for await (let i of data){
+            yield morse.from(i);
+        }
+    },
+    ui,
+    (err => {(err)? console.error(err): console.log('complete');})
+)
